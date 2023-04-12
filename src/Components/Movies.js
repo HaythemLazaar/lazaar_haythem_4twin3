296d1f15d46120074145d95572321f9606907d43
HaythemLazaar <haythem.lazaar1@gmail.com>
2023-04-12 12:51:23 +0100

    search function

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import movies from "../movies.json";
 import Movie from "./Movie";
 
 function Movies() {
+  const [query, setQuery] = useState("");
+  const [list, setList] = useState(movies);
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const search = (e) => {
+    let newList = [];
+    movies.map((m) => {
+      if (m.title.toLowerCase().includes(query.toLowerCase())) {
+        console.log(true);
+        newList.push(m);
+      } else {
+        console.log("not include ");
+      }
+    });
+    setList(newList);
+  };
+
+  useEffect(() => {
+    console.log(list);
+  }, [list]);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {movies.map((m) => {
-        return <Movie movie={m} key={m.id} />;
-      })}
+    <div>
+      <div>
+        <label for="search">Search With title: </label>
+        <input
+          type="text"
+          name="search"
+          placeholder="movie title"
+          onChange={(e) => handleChange(e)}
+        />
+        <button type="submit" onClick={(e) => search(e)}>
+          Submit
+        </button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {list.length >= 1 ? (
+          list.map((m) => {
+            return <Movie movie={m} key={m.id} />;
+          })
+        ) : (
+          <h2>No Result found</h2>
+        )}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 // import { applyMiddleware, createStore } from 'redux';
 // import counter from './Redux/Reducers/counterReducer';
-import { Provider } from "react-redux";
-import { persistStore } from "redux-persist";
+//import { Provider } from "react-redux";
+//import { persistStore } from "redux-persist";
 
 // import logger from 'redux-logger';
 
 // const store = createStore(counter,applyMiddleware(logger));
 //import store from './ReduxToolkit/store'
-import { PersistGate } from "redux-persist/integration/react";
+//import { PersistGate } from "redux-persist/integration/react";
 //const persistor = persistStore(store)
 //store.subscribe(()=>console.log(store.getState()))
 const root = ReactDOM.createRoot(document.getElementById("root"));
